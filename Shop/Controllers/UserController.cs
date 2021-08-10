690776944fb33792890853079b02ddb48f6afdfa
Marilia Mendonça <marilianobre1@gmail.com>
2021-08-10 10:31:16 -0300

    Implementando user controller.

---
+++ Shop/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+using Shop.Services;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [Route("")]
+        [AllowAnonymous]
+        public async Task<ActionResult<User>> Post([FromBody]User model, [FromServices]DataContext context)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                context.Users.Add(model);
+                await context.SaveChangesAsync();
+
+                return Ok(model);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível criar o usuário" });
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody]User model, [FromServices]DataContext context)
+        {
+            var user = await context.Users.AsNoTracking().Where(x => x.Username == model.Username && x.Password == model.Password).FirstOrDefaultAsync();
+            if (user == null)
+                return NotFound(new { message = "Usuário não encontrado" });
+
+            var token = TokenService.GenerateToken(model);
+
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+    }
+}
--- Shop/Services/TokenService.cs
+++ Shop/Services/TokenService.cs
@@ -13,14 +13,15 @@ namespace Shop.Services
         {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(Settings.Secret);
-            var tokenDescription = new SecurityTokenDescriptor{
+            var tokenDescription = new SecurityTokenDescriptor
+            {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
                     new Claim(ClaimTypes.Name, user.Id.ToString()),
                     new Claim(ClaimTypes.Role, user.Role.ToString())
                 }),
                 Expires = DateTime.UtcNow.AddHours(2),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature) 
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
             var token = tokenHandler.CreateToken(tokenDescription);
 
